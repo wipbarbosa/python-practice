d77fa9133fd2c42c424272ff22e6325516c9684f
Guilherme Barbosa <wipbarbosa.dev@gmail.com>
2026-07-30 15:56:41 -0300

    Atualização Review

--- studies/review_lists_01.py
+++ studies/review_lists_01.py
@@ -1,9 +1,38 @@
 lista = []
+numero_inicial = int(input(f"Olá, Digite o 1º  número inteiros: "))
+soma = numero_inicial
+maior = numero_inicial
+menor = numero_inicial
+lista.append(numero_inicial)
 
-for i in range (5):
-    numeros = int(input(f"Olá, Digite 5 numeros inteiros: "))
+for i in range (4):
+    numero = int(input(f"Digite o {i + 2}º  número inteiros: "))
+    lista.append(numero)
+    soma += numero
 
-    lista.append(numeros)
+    if numero <= menor:
+        menor = numero
+    if numero >= maior:
+        maior = numero
 
-print (lista)
+
+
+
+print(f"\nLista digitada: {lista}\n")
+
+for numero in lista:
+
+    if numero %2 == 0:
+        print(f"{numero} é par")
+
+print(
+    f"A soma dos numeros digitados é: {soma}\n"
+    f"O maior numero digitado é {maior}\n"
+    f"O menor numero digitado é {menor}"
+      )
+
+
+
+
+    
 
