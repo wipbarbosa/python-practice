7fc2aa7af3041a82f78a6729737e6f6ac911f27d
Guilherme Barbosa <wipbarbosa.dev@gmail.com>
2026-07-31 17:23:02 -0300

    Add lists and strings review exercise

---
+++ studies/review_03/review_lists_03.py
@@ -0,0 +1,42 @@
+lista = []
+
+
+primeira_palavra = input("Digite a 1ª palavra: ").lower()
+lista.append(primeira_palavra)
+
+palavras_A = 0
+palavras_B = 0
+quantidade_acima = 0
+quantidade_abaixo = 0
+maior_palavra = primeira_palavra
+menor_palavra = primeira_palavra
+
+for i in range(7):
+    palavra_input = input(f"Digita a {i + 2}ª palavra: ").lower()
+    lista.append(palavra_input)
+
+    if len(palavra_input) > len(maior_palavra):
+        maior_palavra = palavra_input
+    if len(palavra_input) < len(menor_palavra):
+        menor_palavra = palavra_input
+
+    if palavra_input.startswith("a"):
+        palavras_A += 1
+    if palavra_input.startswith("b"):
+        palavras_B += 1
+
+    if len(palavra_input) > 5:
+        quantidade_acima += 1
+    if len(palavra_input) <= 5:
+        quantidade_abaixo += 1
+
+
+print(f"Lista de palavras: ")
+print(f"{lista}")
+
+print (f"Quantidade de palvras acima de 5 letras: {quantidade_acima}")
+print (f"Quantidade de palavras abaixo de 5 letras: {quantidade_abaixo}")
+print (f"Quantidade de palavras começadas com a letra A: {palavras_A}")
+print (f"Quantidade de palavras começadas com a letra B :{palavras_B}")
+print (f"Maior palavra: {maior_palavra}")
+print (f"Menor palavra: {menor_palavra}")
