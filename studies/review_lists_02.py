458a6b0400242e00b3a515b0055878ab6c952f3e
Guilherme Barbosa <wipbarbosa.dev@gmail.com>
2026-07-31 15:49:04 -0300

    Uptade review list 2

--- studies/review_lists_02.py
+++ studies/review_lists_02.py
@@ -1,8 +1,26 @@
 lista = []
 
-for i in range(5):
-    nomes = input("Digite 5 nomes")
+primeiro_nome = input("Digite o 1º Nome: ").lower()
+lista.append(primeiro_nome)
+
+maior_nome = len(primeiro_nome)
+menor_nome = len(primeiro_nome)
+
+for i in range(4):
+    nomes = input(f"Digite o {i + 2}º nome: ").lower()
     lista.append(nomes)
 
+print(f"==Lista de nomes==\n")
+for nomes in lista:
+    print(f"--{nomes}\n"
+          f"({"=" * 20})"
+          )
+print(f"({"=" * 20})")
+    
 
-print (lista)
+for nomes in lista:
+    if len(nomes) > 5:
+        print(f"{nomes}: tem mais que 5 letras.")
+
+print (maior_nome)
+print (menor_nome)
