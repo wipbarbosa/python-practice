df8824e92a1041753f60e0e333cc17de1888cbed
Guilherme Barbosa <wipbarbosa.dev@gmail.com>
2026-07-31 17:27:28 -0300

    Add lists and strings review exercise

--- studies/review_03/review_lists_03.py
+++ studies/review_03/review_lists_03.py
@@ -1,15 +1,27 @@
 lista = []
 
-
-primeira_palavra = input("Digite a 1ª palavra: ").lower()
-lista.append(primeira_palavra)
-
 palavras_A = 0
 palavras_B = 0
 quantidade_acima = 0
 quantidade_abaixo = 0
+
+
+primeira_palavra = input("Digite a 1ª palavra: ").lower()
+lista.append(primeira_palavra)
+
 maior_palavra = primeira_palavra
 menor_palavra = primeira_palavra
+
+if primeira_palavra.startswith("a"):
+    palavras_A += 1
+if primeira_palavra.startswith("b"):
+    palavras_B += 1
+
+if len(primeira_palavra) > 5:
+    quantidade_acima += 1
+if len(primeira_palavra) <= 5:
+    quantidade_abaixo += 1
+
 
 for i in range(7):
     palavra_input = input(f"Digita a {i + 2}ª palavra: ").lower()
