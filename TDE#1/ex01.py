28352539d2d170fc4b9a31741f2a771f2f582cd8
Guilherme Barbosa <wipbarbosa.dev@gmail.com>
2026-08-05 17:26:38 -0300

    atualização

---
+++ TDE#1/ex01.py
@@ -0,0 +1,11 @@
+#1. Faça um algoritmo que leia um número inteiro e escreva seu antecessor e sucessor.
+print(f"===== ANTECESSOR E SUCESSOR =====\n")
+
+numero = int(input("Digite um numero desejado: "))
+
+print(f"Antecessor: {numero - 1}\n" 
+      f"Sucessor: {numero + 1}"
+    )
+
+
+
