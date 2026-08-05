deb3fa89131d32e566a45d5fe9d122786927b67a
Guilherme Barbosa <wipbarbosa.dev@gmail.com>
2026-08-05 17:37:38 -0300

    Atualização de portifólio

---
+++ TDE#1/ex02.py
@@ -0,0 +1,10 @@
+#2. Faça um algoritmo que leia o ano de nascimento de uma pessoa e calcule a idade que completará até o final de 2025.
+
+print("===== IDADE DO USUÁRIO AO FINAL DO ANO DE 2025 =====\n")
+
+ANO_REFERENCIA = 2025
+
+ano_nascimento = int(input("Digite o ano do seu nascimento: "))
+
+print(f"\nSua idade ano final do ano de 2025 será de {ANO_REFERENCIA - ano_nascimento}")
+print("=" * 40)
