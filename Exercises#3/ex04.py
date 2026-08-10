4bb535f71b2705712d0fc87d7746003a13b5eb43
Guilherme Barbosa <wipbarbosa.dev@gmail.com>
2026-08-10 15:26:14 -0300

    Atualização de portifólio

---
+++ Exercises#3/ex04.py
@@ -0,0 +1,23 @@
+# 4. Leia dois valores reais do teclado, calcular e imprimir na tela:
+# a) A soma destes valores b) O produto deles c) O quociente entre eles
+
+print(f"===== PROGRAMA DE SOMA, PRODUTO E QUOCIENTE DE DOIS VALORES REAIS INTEIROS =====")
+
+numero1 = int(input(f'Digite o primeiro número: '))
+numero2 = int(input(f'Digite o segundo número: '))
+
+soma = numero1 + numero2
+produto = numero1 * numero2
+quociente = numero1 / numero2
+
+
+
+print(
+    f'===== RESULTADO =====\n'
+    f'Soma: {soma}\n'
+    f'Produto: {produto}\n'
+    f'Quociente: {quociente}'
+    )
+
+
+
