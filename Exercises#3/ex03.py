5bfc3ab07ae11766f06c6c9c9dee9159a3090dc7
Guilherme Barbosa <wipbarbosa.dev@gmail.com>
2026-08-10 15:26:32 -0300

    Atualização de portifólio

--- Exercises#3/ex03.py
+++ Exercises#3/ex03.py
@@ -1,19 +1,13 @@
 # 3. Leia três números do teclado e verificar se o primeiro é maior que a soma dos outros dois.
 
-entrada = int(input('Digite o 1º número inteiro: '))
+primeiro = int(input('Digite o 1º número inteiro: '))
 soma = 0
 
 for i in range (2):
-    numero1 = entrada
+    numero = int(input(f'Digite o {i +2}º número inteiro: '))
+    soma += numero
 
-    numeros = int(input(f'Digite o {i +2}º número inteiro: '))
-
-    soma += numeros
-
-if soma > numero1:
-    print (f"A soma dos dois segundos {soma} numeros é meior que o primeiro{entrada}")
+if primeiro > soma:
+    print(f"O primeiro número ({primeiro}) é maior que a soma dos outros dois ({soma}).")
 else:
-    print(f"A soma dos dois segundos numeros {soma} é menor que o primeiro {entrada}")
-
-
-
+    print(f"O primeiro número ({primeiro}) não é maior que a soma dos outros dois ({soma}).")
--- Exercises#3/ex04.py
+++ Exercises#3/ex04.py
@@ -1,10 +1,10 @@
 # 4. Leia dois valores reais do teclado, calcular e imprimir na tela:
 # a) A soma destes valores b) O produto deles c) O quociente entre eles
 
-print(f"===== PROGRAMA DE SOMA, PRODUTO E QUOCIENTE DE DOIS VALORES REAIS INTEIROS =====")
+print(f"===== PROGRAMA DE SOMA, PRODUTO E QUOCIENTE DE DOIS VALORES REAIS =====")
 
-numero1 = int(input(f'Digite o primeiro número: '))
-numero2 = int(input(f'Digite o segundo número: '))
+numero1 = float(input(f'Digite o primeiro número: '))
+numero2 = float(input(f'Digite o segundo número: '))
 
 soma = numero1 + numero2
 produto = numero1 * numero2
