a4c9bae81db591cce898331c2de9574f63a99847
Guilherme Barbosa <wipbarbosa.dev@gmail.com>
2026-08-11 16:26:38 -0300

    Atualização de portifólio

--- Exercises#3/ex12.py
+++ Exercises#3/ex12.py
@@ -1,6 +1,6 @@
-# 12. Desenvolva um programa que leia um vetor de 20 posições inteiras e o coloque em ordem crescente, utilizando a seguinte
+# 12. Desenvolva um programa que leia um lsita de 20 posições inteiras e o coloque em ordem crescente, utilizando a seguinte
 # estratégia de ordenação:
-# • selecione o elemento do vetor de 20 posições que apresenta o menor valor;
+# • selecione o elemento do lsita de 20 posições que apresenta o menor valor;
 # • troque este elemento pelo primeiro;
 # • repita estas operações, envolvendo agora apenas os 19 elementos restantes (trocando o de menor valor com a segunda
 # posição), depois os 18 elementos (trocando o de menor valor com a terceira posição), depois os 17, 16 e assim por diante,
@@ -8,21 +8,20 @@
 # Observação: este método de ordenação é conhecido como “Seleção Direta”
 
 lista = []
-menor_valor = 
 
-while len(lista) < 5:
-    numero = int(input('Digite 20 valores inteiros: '))
+for i in range(20):
+    numero = int(input(f"Digite o {i + 1}º número: "))
     lista.append(numero)
 
+for i in range(len(lista) - 1):
 
-while len(lista_ordenada) < 5:
-    for numero in lista:
-        if menor_valor < numero:
-            menor_valor = numero
-            lista_ordenada.append(menor_valor)
-        
-        
+    menor = i
+
+    for j in range(i + 1, len(lista)):
+        if lista[j] < lista[menor]:
+            menor = j
+
+    lista[i], lista[menor] = lista[menor], lista[i]
+
 
 print(lista)
-print(lista_ordenada)
-print(menor_valor)
