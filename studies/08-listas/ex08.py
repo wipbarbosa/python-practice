277a03280d3d824b33b4465baa1e12adf33d6464
Guilherme Barbosa <wipbarbosa.dev@gmail.com>
2026-08-11 17:54:23 -0300

    Atualização de portifólio

---
+++ studies/08-listas/ex08.py
@@ -0,0 +1,40 @@
+'''nomes = ["Ana", "Carlos", "Pedro", "Maria", "João"]
+
+for i , nome in enumerate(nomes):
+    if i  % 2 ==  0:
+        print (nome)'''
+
+'''numeros = [10, 5, 20, 8]
+
+print(sum(numeros))
+print(min(numeros))
+print(max(numeros))'''
+
+'''numeros = [1, 2, 3, 4, 5, 6]
+
+dobro = [numero + numero for numero in numeros]
+print (dobro)'''
+
+'''numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+pares = [numero for numero in numeros if numero % 2 == 0 ]
+
+print (pares)
+
+impares = [numero for numero in numeros if numero % 2 == 1 ]
+
+print (impares)'''
+
+
+'''numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+maiores = [numero for numero  in numeros if numero > 5]
+
+print (maiores)'''
+
+
+numeros = [1, 2, 3, 4, 5, 6]
+
+dobro_pares = [numero * 2 for numero in numeros if numero % 2 == 0]
+
+print(dobro_pares)
