a702e37614d50cc0d0908263cddba635dbdbca49
Guilherme Barbosa <wipbarbosa.dev@gmail.com>
2026-08-12 17:48:06 -0300

    Atualização de portifólio

---
+++ studies/11-matriz/ex06.py
@@ -0,0 +1,33 @@
+matriz = []
+soma = 0
+
+for i in range(2):
+    linha = []
+    print(f'{i + 1}ª linha.')
+
+    for j in range(2):
+        numero = int(input(f'Digite o {j + 1}º número: '))
+        linha.append(numero)
+
+    matriz.append(linha)
+
+maiores = []
+
+for j in range(2):
+    maior = matriz[0][j]
+
+    for i in range(2):
+        if matriz[i][j] > maior:
+            maior = matriz[i][j]
+
+    maiores.append(maior)
+
+for numero in maiores:
+    soma += numero
+
+media = soma / len(maiores)
+
+print(matriz)
+print(maiores)
+print(soma)
+print(media)
