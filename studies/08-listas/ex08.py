5ef1157ba2f2248fe721ac40238cb3805e67d885
Guilherme Barbosa <wipbarbosa.dev@gmail.com>
2026-08-12 14:46:42 -0300

    Atualização de portifólio

--- studies/08-listas/ex08.py
+++ studies/08-listas/ex08.py
@@ -33,8 +33,23 @@ maiores = [numero for numero  in numeros if numero > 5]
 print (maiores)'''
 
 
-numeros = [1, 2, 3, 4, 5, 6]
+'''numeros = [1, 2, 3, 4, 5, 6]
 
 dobro_pares = [numero * 2 for numero in numeros if numero % 2 == 0]
 
-print(dobro_pares)
+print(dobro_pares)'''
+
+import random
+
+lista = []
+
+while True:
+    numero = random.randint(1, 10)
+    
+    if numero not in lista:
+        lista.append(numero)
+
+    if len(lista) == 10:
+        break
+    
+print(lista)
