ec2f094039a671c501b9c96d9258e85a2120acd1
Guilherme Barbosa <wipbarbosa.dev@gmail.com>
2026-08-18 16:33:15 -0300

    Atualização de portifólio

---
+++ studies/11-matriz/ex08.py
@@ -0,0 +1,31 @@
+matriz = []
+
+for i in range(3):
+    linha = []
+
+    print(f'{i + 1}ª linha')
+    for j in range(3):
+        numero = int(input(f"Digite o {j+1}º numero: "))
+        linha.append(numero)
+
+    matriz.append(linha)
+
+
+for i in range(3):
+    maior = matriz [i][0]
+    for j in range(3):
+        if matriz [i][j] > maior:
+            maior = matriz [i][j]
+
+    print(f'Maior da linha {i + 1}: {maior}')
+
+for j in range(3):
+    maior = matriz [0][j]
+    for i in range(3):
+        if matriz [i][j] > maior:
+            maior = matriz [i][j]
+
+    print(f'Maior da coluna {j + 1}: {maior}')
+
+
+
